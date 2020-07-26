514f94681b5e3e92f3a524aba72c5ec6d57237bf
Dimplesai234 <dimplesainandeti@gmail.com>
2020-07-26 19:57:34 +0530

    Updates

---
+++ precache-manifest.b8735b9bf0283831f36f5dd7898b31c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59978f78243fa8a55049797b7a5de8ca",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "21953e0ed39295943492",
+    "url": "/expense-tracker/static/css/main.741b4db0.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21953e0ed39295943492",
+    "url": "/expense-tracker/static/js/main.aa7e840c.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
